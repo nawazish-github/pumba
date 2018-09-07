91caf84b712bfc6ed1485d13ce59cf639582ccac
Alexei Ledenev <alexei.led@gmail.com>
2018-09-07 17:04:47 +0300

    working with codebuild

--- hack/codebuild_build.sh
+++ hack/codebuild_build.sh
@@ -1,35 +1,59 @@
-#!/bin/bash
+#!/bin/sh
 
-readonly repo=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPO_NAME}
-readonly branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
-readonly commit=$(git rev-parse --short HEAD 2>/dev/null)
+CODEBUILD_GIT_BRANCH="$(git symbolic-ref HEAD --short 2>/dev/null)"
+if [ "$CODEBUILD_GIT_BRANCH" = "" ]; then
+  CODEBUILD_GIT_BRANCH="$(git branch -a --contains HEAD | sed -n 2p | awk '{ printf $1 }')";
+  CODEBUILD_GIT_BRANCH=${CODEBUILD_GIT_BRANCH#remotes/origin/};
+fi
+
+readonly account=$(aws sts get-caller-identity --query 'Account' --output text)
+readonly repo=${account}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPO_NAME}
+readonly branch=${CODEBUILD_GIT_BRANCH}
+readonly commit=${CODEBUILD_SOURCE_VERSION}
 readonly version=$(cat VERSION)
+readonly build_id=${CODEBUILD_BUILD_ID}
+readonly build_url=https://$AWS_REGION.console.aws.amazon.com/codebuild/home?region=$AWS_REGION#/builds/${build_id}/view/new
+
+
+echo "=> current branch:commit = ${branch}:${commit}"
 
 # Attempt to pull existing builder image
-if docker pull ${repo}:builder-branch; then
+echo "=> try getting previous builder image: ${repo}:builder-${branch}" 
+if docker pull ${repo}:builder-${branch}; then
     # Update builder image
-    docker build -t ${repo}:builder-branch --target builder --cache-from ${repo}:builder-branch .
+    echo "=> updating builder image: ${repo}:builder-${branch}" 
+    docker build -t ${repo}:builder-${branch} --target builder --cache-from ${repo}:builder-${branch} -f docker/Dockerfile .
 else
     # Create new builder image
-    docker build -t ${repo}:builder-branch --target builder .
+    echo "=> creating new builder image: ${repo}:builder-${branch}" 
+    docker build -t ${repo}:builder-${branch} --target builder -f docker/Dockerfile .
 fi
 
 # Attempt to pull latest branch target image
-docker pull ${repo}:branch || true
+echo "=> try getting previous final image: ${repo}:${branch}" 
+docker pull ${repo}:${branch} || true
 
 # Build and push target image
-docker build -t ${repo}:branch --cache-from ${repo}:builder-branch --cache-from ${repo}:branch \
+echo "=> try getting previous final image: ${repo}:${branch}" 
+docker build -t ${repo}:${branch} --cache-from ${repo}:builder-${branch} --cache-from ${repo}:${branch} \
   --build-arg GH_SHA=${commit} \
   --build-arg GITHUB_TOKEN=$GITHUB_TOKEN \
-  --build-arg CODECOV_TOKEN=$CODECOV_TOKEN
+  --build-arg CODECOV_TOKEN=$CODECOV_TOKEN \
+  --build-arg VCS_COMMIT_ID=${commit} \
+  --build-arg VCS_BRANCH_NAME=${branch} \
+  --build-arg CI_BUILD_ID=${build_id} \
+  --build-arg CI_BUILD_URL=${build_url} \
   -f docker/Dockerfile .
 
-docker push ${repo}:branch
+echo "=> push final image: ${repo}:${branch}" 
+docker push ${repo}:${branch}
 
 # for master push versioned image too
-if [ "${version}" == "master" ]; then
-    docker push ${repo}:version
+if [ "${branch}" == "master" ]; then
+    echo "=> push final versioned image: ${repo}:${version}" 
+    docker push ${repo}:${version}
 fi
 
 # Push builder image
-docker push ${repo}:builder-branch
+echo "=> push builder image for builder cache: ${repo}:builder-${branch}" 
+docker push ${repo}:builder-${branch}
